eb650777db52bd3be2ca674c32d4006a8054d518
EuanHam <euanham05@gmail.com>
2024-09-28 17:25:12 -0400

    Added to tasks. There are default tasks in, but do not save across navigation

--- bloom/src/Tasks.js
+++ bloom/src/Tasks.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tasks.css';
 import backButton from './images/back_button.png';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; // Import BrowserRouter
-
+import { Link } from 'react-router-dom';
 
 function Tasks() {
+  const [tasks, setTasks] = useState(["Shower", "Meditation", "Drink Water", "Journal"]);
+  const [date, setDate] = useState(new Date());
+
+  const addTask = () => {
+    const newTask = prompt("What task would you like to add?");
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+    }
+  };
+
   return (
     <div className="Tasks">
       <header className="Tasks-header">
         <Link to="/" onClick={() => { console.log("Tasks button clicked!") }}>
-          <img src={backButton} alt="Back Button" style={{ width: '150px', height: 'auto', cursor: 'pointer', marginRight: 'auto', display: 'block'}} />
+          <img src={backButton} alt="Back Button" className="back-button" />
         </Link>
-        <p>Figure out how we wanna keep track of just daily tasks</p>
-       </header>
-     </div>
-  )
+        <p>Today is {date.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
+        <div className="task-grid">
+          {tasks.map((task, index) => (
+            <div key={index} className="task-box">{task}</div>
+          ))}
+          <div className="task-box add-task" onClick={addTask}>+</div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Tasks;
