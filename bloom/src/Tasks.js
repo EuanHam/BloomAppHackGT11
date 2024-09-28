f47795819429cfef324bf68ccd70c949b69c11bb
EuanHam <euanham05@gmail.com>
2024-09-28 17:02:36 -0400

    Added custom art

--- bloom/src/Tasks.js
+++ bloom/src/Tasks.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Tasks.css';
+import backButton from './images/back_button.png';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; // Import BrowserRouter
 
 
 function Tasks() {
   return (
     <div className="Tasks">
-       <header className="Tasks-header">
-         <p>Figure out how we wanna keep track of just daily tasks</p>
+      <header className="Tasks-header">
+        <Link to="/" onClick={() => { console.log("Tasks button clicked!") }}>
+          <img src={backButton} alt="Back Button" style={{ width: '150px', height: 'auto', cursor: 'pointer', marginRight: 'auto', display: 'block'}} />
+        </Link>
+        <p>Figure out how we wanna keep track of just daily tasks</p>
        </header>
      </div>
   )
