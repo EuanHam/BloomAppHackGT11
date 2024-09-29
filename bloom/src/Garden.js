1fc0023b1e035343abf5c3acee7085119b55bddb
EuanHam <euanham05@gmail.com>
2024-09-29 00:01:49 -0400

    Fixed garden constraints

--- bloom/src/Garden.js
+++ bloom/src/Garden.js
@@ -7,9 +7,9 @@ import potImage from './images/flower_pot.png';
 function Garden() {
 
   const [pots, setPots] = useState([
-    { id: 1, x: 700, y: 600, size: 300, flower: ''}, // Pot 1
-    { id: 2, x: 300, y: 600, size: 300, flower: ''}, // Pot 2
-    { id: 3, x: 1100, y: 600, size: 300,flower: '' }, // Pot 3 
+    { id: 1, position: '20%', size: 300, flower: ''}, // Pot 1 at 20%
+    { id: 2, position: '50%', size: 300, flower: ''}, // Pot 2 at 50%
+    { id: 3, position: '80%', size: 300, flower: '' }, // Pot 3 at 80%
   ]);
 
   const [selectedPot, setSelectedPot] = useState(null);
@@ -33,31 +33,40 @@ function Garden() {
     <div className="Garden">
       <header className="Garden-header">
         <Link to="/" onClick={() => { console.log("Tasks button clicked!") }}>
-          <img src={backButton} alt="Back Button" style={{ width: '150px', height: 'auto', cursor: 'pointer'}} />
+          <img src={backButton} alt="Back Button" style={{ width: '150px', height: 'auto', cursor: 'pointer' }} />
         </Link>
 
-        {pots.map((pot, index) => (
-          <div key={pot.id} style={{ position: 'absolute', left: `${pot.x}px`, top: `${pot.y}px` }}>
-          <img 
-          src={potImage}
-          alt="Pot"
-          className="pot"
-          style={{
-            width: `${pot.size}px`, 
-            height: 'auto',
-            cursor: 'pointer',
-            position: 'absolute'
-          }}
-          onClick={() => handlePotClick(pot)}
-          />
-          </div>
-        ))}
+        {/* Pot Container */}
+        <div className="pot-container">
+          {pots.map((pot) => (
+            <div 
+              key={pot.id} 
+              style={{ 
+                position: 'absolute', 
+                left: pot.position, 
+                bottom: '10px', 
+                transform: 'translateX(-50%)'  // Center the pot at the percentage point
+              }}>
+              <img 
+                src={potImage}
+                alt="Pot"
+                className="pot"
+                style={{
+                  width: `${pot.size}px`, 
+                  height: 'auto',
+                  cursor: 'pointer',
+                }}
+                onClick={() => handlePotClick(pot)}
+              />
+            </div>
+          ))}
+        </div>
 
         {selectedPot && (
           <div className="popup" style={{ 
             position: 'absolute', 
-            left: `${selectedPot.x + 50}px`, 
-            top: `${selectedPot.y}px`
+            left: `${selectedPot.position}`, 
+            top: `${selectedPot.size + 10}px` /* Adjust popup positioning */
           }}>
             <p>Select a flower for this pot:</p>
             {flowers.map((flower) => (
@@ -69,7 +78,7 @@ function Garden() {
         )}
       </header>
     </div>
-  )
+  );
 }
 
 export default Garden;
